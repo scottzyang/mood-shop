3671a94b4d7f6bcde89d2de762bedd1dfa43be46
scottzyang <scottzyang@gmail.com>
2022-09-20 20:43:25 -0500

    displayed cart items on console

--- scripts.js
+++ scripts.js
@@ -1,53 +1,92 @@
-import data from './data.js'
+import data from "./data.js";
 
-const itemsContainer = document.querySelector('#items')
+const itemsContainer = document.querySelector("#items");
 
 // the length of our data determines how many times this loop goes around
 const jsonLength = data.length;
 
-
 for (let i = 0; i < data.length; i += 1) {
-	// create a new div element and give it a class name
-	const newDiv = document.createElement('div');
-	newDiv.className = 'item'
-	// create an image element
-	const img = document.createElement('img');
-	// this will change each time we go through the loop. Can you explain why?
-	img.src = data[i].image
-	img.width = 300
-	img.height = 300
-	// Add the image to the div
-	newDiv.appendChild(img)
-	console.log(img) // Check the console!
-	itemsContainer.appendChild(newDiv)
+  // create a new div element and give it a class name
+  const newDiv = document.createElement("div");
+  newDiv.className = "item";
+  // create an image element
+  const img = document.createElement("img");
+  // this will change each time we go through the loop. Can you explain why?
+  img.src = data[i].image;
+  img.width = 300;
+  img.height = 300;
+  // Add the image to the div
+  newDiv.appendChild(img);
+  console.log(img); // Check the console!
+  itemsContainer.appendChild(newDiv);
 
-    // create a p tag element for description from data.js file
-    const desc = document.createElement('P')
-    desc.innerText = data[i].desc
-    newDiv.appendChild(desc)
+  // create a p tag element for description from data.js file
+  const desc = document.createElement("P");
+  desc.innerText = data[i].desc;
+  newDiv.appendChild(desc);
 
-    // create a p tag element for price from data.js
-    const price = document.createElement('P')
-    price.innerText = data[i].price
-    newDiv.appendChild(price)
+  // create a p tag element for price from data.js
+  const price = document.createElement("P");
+  price.innerText = data[i].price;
+  newDiv.appendChild(price);
 
-    // create a button with text 'Add to cart'
-    const button = document.createElement('button')
-    button.id = data[i].name
+  // create a button with text 'Add to cart'
+  const button = document.createElement("button");
+  button.id = data[i].name;
 
-    //creates custom attribute called data-price and extracts the price value from data.js
-    button.dataset.price = data[i].price
-    button.innerHTML = 'Add to Cart'
-    newDiv.appendChild(button)
+  //creates custom attribute called data-price and extracts the price value from data.js
+  button.dataset.price = data[i].price;
+  button.innerHTML = "Add to Cart";
+  newDiv.appendChild(button);
 }
 
 const cart = [];
 
+// add items to cart
 function addItem(name, price) {
-    const item = {name: name, price: price, qty: 1}
-    cart.push(item)
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].name === name) {
+      cart[i].qty += 1;
+      return;
+    }
+  }
+  const item = { name, price, qty: 1 };
+  cart.push(item);
 }
 
+//show items
 function showItems() {
-    console.log(`You have ${cart.length} items in your cart`)
+  console.log(`You have ${getQty()} items in your cart`);
+
+  for (let i = 0; i < cart.length; i++) {
+    console.log(`-  ${cart[i].name} ${cart[i].price} x ${cart[i].qty}`);
+  }
+  console.log(`Total in cart: $${getTotal()}`);
 }
+
+// get totals
+function getTotal() {
+  let total = 0;
+  for (let i = 0; i < cart.length; i += 1) {
+    total += cart[i].price * cart[i].qty;
+  }
+  return total.toFixed(2);
+}
+
+// get quantities
+function getQty() {
+  let qty = 0;
+
+  for (let i = 0; i < cart.length; i += 1) {
+    qty += cart[i].qty;
+  }
+  return qty;
+}
+
+addItem("Apple", 0.99);
+addItem("Apple", 0.99);
+addItem("Apple", 0.99);
+addItem("Orange", 1.29);
+addItem("Apple", 0.99);
+
+showItems();
